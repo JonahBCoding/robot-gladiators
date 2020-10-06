ab7513e3d043472eb239644690b8e90d8238e34b
unknown <jonah.benson@gmail.com>
2020-10-05 18:43:46 -0700

    k

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,11 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames.length);
+for(var i = 0; i < 3; i++) {
+    console.log("apple", i);
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -23,15 +27,15 @@ var fight = function () {
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
    
     // Check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     }
     else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left. ");
     }
 
     // Remove player's health by subtracting the amount set in the enemyAttack variable
@@ -39,7 +43,7 @@ var fight = function () {
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
     // check player's health
@@ -68,3 +72,11 @@ var fight = function () {
 }    
   //call the function:
   fight();
+
+  
+
+  // Game States
+  //"WIN" - Player robot has deafeated all enemy robots
+  //        * Fight all enemy robots
+  //        * Defeat each enemy robot
+  //"LOSE" - Player robot's health is zero or less
